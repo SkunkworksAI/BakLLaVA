afd4c6b00290d225e6e614284dd015019e5ee331
Far El <36641995+pharaouk@users.noreply.github.com>
2023-10-11 16:38:58 +0300

    Update finetune_full_schedule.sh

--- scripts/finetune_full_schedule.sh
+++ scripts/finetune_full_schedule.sh
@@ -12,12 +12,16 @@
 # MODEL_VERSION="llama-2-7b-chat"
 ################## LLaMA-2 ##################
 
+PROMPT_VERSION="llava_llama_2"
+MODEL_VERSION=mistralai/Mistral-7B-v0.1
+
+
 deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero2.json \
-    --model_name_or_path ./checkpoints/$MODEL_VERSION \
+    --model_name_or_path $MODEL_VERSION \
     --version $PROMPT_VERSION \
-    --data_path ./playground/data/llava_instruct_158k.json \
-    --image_folder /path/to/coco/train2017 \
+    --data_path ./finetune_data/chat/llava_instruct_158k.json \
+    --image_folder ./finetune_data/images \
     --vision_tower openai/clip-vit-large-patch14 \
     --pretrain_mm_mlp_adapter ./checkpoints/llava-$MODEL_VERSION-pretrain/mm_projector.bin \
     --mm_vision_select_layer -2 \
@@ -31,7 +35,7 @@ deepspeed llava/train/train_mem.py \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 50000 \
+    --save_steps 5000 \
     --save_total_limit 1 \
     --learning_rate 2e-5 \
     --weight_decay 0. \
@@ -39,8 +43,8 @@ deepspeed llava/train/train_mem.py \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
     --tf32 True \
-    --model_max_length 2048 \
+    --model_max_length 8000 \
     --gradient_checkpointing True \
-    --dataloader_num_workers 4 \
+    --dataloader_num_workers 8 \
     --lazy_preprocess True \
     --report_to wandb
