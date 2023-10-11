45c225f280360a58890e0d490a5c6e6e3665a7b8
Far El <36641995+pharaouk@users.noreply.github.com>
2023-10-11 16:56:31 +0300

    Update finetune.sh

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -16,21 +16,25 @@ PROMPT_VERSION="llava_llama_2"
 MODEL_VERSION=mistralai/Mistral-7B-v0.1
 
 
+#!/bin/bash
 
 deepspeed llava/train/train_mem.py \
-    --deepspeed ./scripts/zero2.json \
+    --deepspeed ./scripts/zero3.json \
     --model_name_or_path $MODEL_VERSION \
-    --version $PROMPT_VERSION \
+    --version v1 \
     --data_path finetune_data/chat/llava_instruct_150k.json \
     --image_folder finetune_data/images \
-    --vision_tower openai/clip-vit-large-patch14 \
-    --pretrain_mm_mlp_adapter ./checkpoints/llava-$MODEL_VERSION-pretrain/mm_projector.bin \
+    --vision_tower openai/clip-vit-large-patch14-336 \
+    --pretrain_mm_mlp_adapter ./checkpoints/llava-mistralai/Mistral-7B-v0.1-pretrain/mm_projector.bin \
+    --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
     --bf16 True \
-    --output_dir ../n6/checkpoints/llava-$MODEL_VERSION-finetune \
-    --num_train_epochs 3 \
+    --output_dir ../n8/checkpoints/mistral-llava \
+    --num_train_epochs 1 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
@@ -39,13 +43,45 @@ deepspeed llava/train/train_mem.py \
     --save_steps 5000 \
     --save_total_limit 1 \
     --learning_rate 2e-5 \
-    --weight_decay 0.01 \
+    --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
     --tf32 True \
     --model_max_length 8000 \
     --gradient_checkpointing True \
-    --dataloader_num_workers 8 \
+    --dataloader_num_workers 4 \
     --lazy_preprocess True \
     --report_to wandb
+# deepspeed llava/train/train_mem.py \
+#     --deepspeed ./scripts/zero2.json \
+#     --model_name_or_path $MODEL_VERSION \
+#     --version $PROMPT_VERSION \
+#     --data_path finetune_data/chat/llava_instruct_150k.json \
+#     --image_folder finetune_data/images \
+#     --vision_tower openai/clip-vit-large-patch14 \
+#     --pretrain_mm_mlp_adapter ./checkpoints/llava-$MODEL_VERSION-pretrain/mm_projector.bin \
+#     --mm_vision_select_layer -2 \
+#     --mm_use_im_start_end False \
+#     --mm_use_im_patch_token False \
+#     --bf16 True \
+#     --output_dir ../n6/checkpoints/llava-$MODEL_VERSION-finetune \
+#     --num_train_epochs 3 \
+#     --per_device_train_batch_size 16 \
+#     --per_device_eval_batch_size 4 \
+#     --gradient_accumulation_steps 1 \
+#     --evaluation_strategy "no" \
+#     --save_strategy "steps" \
+#     --save_steps 5000 \
+#     --save_total_limit 1 \
+#     --learning_rate 2e-5 \
+#     --weight_decay 0.01 \
+#     --warmup_ratio 0.03 \
+#     --lr_scheduler_type "cosine" \
+#     --logging_steps 1 \
+#     --tf32 True \
+#     --model_max_length 8000 \
+#     --gradient_checkpointing True \
+#     --dataloader_num_workers 8 \
+#     --lazy_preprocess True \
+#     --report_to wandb
