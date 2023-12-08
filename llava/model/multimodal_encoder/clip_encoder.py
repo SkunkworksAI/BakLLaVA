ee758effe054ad33433cea58962d287f93213e52
Marianna Nezhurina <nezhurina1@jsfl03.jusuf>
2023-12-08 18:18:08 +0100

    bug fix

--- llava/model/multimodal_encoder/clip_encoder.py
+++ llava/model/multimodal_encoder/clip_encoder.py
@@ -50,6 +50,8 @@ class CLIPVisionTower(nn.Module):
             self.image_processor = get_open_clip_image_processor(self.vision_tower_name)
         else:
             self.vision_tower = CLIPVisionModel.from_pretrained(self.vision_tower_name)
+
+        self.image_processor = CLIPImageProcessor(self.vision_tower_name)
         self.vision_tower.requires_grad_(False)
 
         self.is_loaded = True
